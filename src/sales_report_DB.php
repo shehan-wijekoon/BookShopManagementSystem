27599cccc78ce8c8b2929fcac8a0465f08f0840b
supun sri wickrama <mnsupiri23@gmail.com>
2024-10-12 09:20:47 +0530

    cmmnt

--- src/sales_report_DB.php
+++ src/sales_report_DB.php
@@ -4,19 +4,18 @@ $username = "root";     // Your database username
 $password = "";     // Your database password
 $dbname = "bookshop";  // Your database name
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL query to select all records from your table
+
 $sql = "SELECT date, id, name, price, quantity, total FROM sales_report";
 $result = $conn->query($sql);
 
-// Output the records in table format
 if ($result->num_rows > 0) {
     echo "<table>";
     echo "<thead>                        <th>Date</th>
@@ -26,7 +25,7 @@ if ($result->num_rows > 0) {
                         <th>quantity</th>
                         <th>total</th></tr><thead>";
 
-    // Loop through the records and create table rows
+
     while ($row = $result->fetch_assoc()) {
         echo "<tbody><tr><td>" . $row["date"] . "</td><td>" . $row["id"] . "</td><td>" . $row["name"] ."</td><td>".  $row["price"]."</td><td>".$row["quantity"]."</td><td>".$row["total"]." </td></tr><tbody>";
     }
