7ae0c7c462bba6e60317f6484e27817785527bc6
supun sri wickrama <mnsupiri23@gmail.com>
2024-10-12 09:23:27 +0530

    sadada

--- src/delete.php
+++ src/delete.php
@@ -4,18 +4,16 @@ $username = "root";
 $password = "";
 $dbname = "bookshop";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get the record ID from URL
 $id = $_GET['id'];
 
-// Delete query
+
 $sql = "DELETE FROM inventory WHERE code=$id";
 
 if ($conn->query($sql) === TRUE) {
@@ -24,10 +22,8 @@ if ($conn->query($sql) === TRUE) {
     echo "Error deleting record: " . $conn->error;
 }
 
-// Close the connection
 $conn->close();
 
-// Redirect back to the display page after deletion
 header("Location: inventoryy.php");
 exit;
 ?>
--- src/inventory_table_view.php
+++ src/inventory_table_view.php
@@ -4,19 +4,15 @@ $username = "root";     // Your database username
 $password = "";     // Your database password
 $dbname = "bookshop";  // Your database name
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL query to select all records from your table
 $sql = "SELECT code, name, quantity, price FROM inventory";
 $result = $conn->query($sql);
 
-// Output the records in table format
 if ($result->num_rows > 0) {
     echo "<table>";
     echo "<thead>                        <th>Item Code</th>
@@ -25,7 +21,6 @@ if ($result->num_rows > 0) {
                         <th>Price</th>
                         <th>Actions</th></tr><thead>";
 
-    // Loop through the records and create table rows
     while ($row = $result->fetch_assoc()) {
         echo "<tbody><tr><td>" . $row["code"] . "</td><td>" . $row["name"] . "</td><td>" . $row["quantity"] ."</td><td>".  $row["price"]."</td><td>"."  <a href='Stock.php' class='edit-btn'>Edit</a>
                             <a href='delete.php?id=" . $row["code"] . "'  class='del-btn'>Delete</a></td></tr><tbody>";
